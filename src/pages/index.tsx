5eaf72e5133c078f731f0c469c00ac850503fa74
Abraham Avila <aavilas75@hotmail.com>
2023-02-08 19:17:13 -0600

    Ecommerce API Call setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
-import Head from 'next/head'
-import { Heading } from '@chakra-ui/react'
-export default function Home() {
+import { log } from "console";
+import { GetServerSidePropsContext } from "next";
+import Head from "next/head";
 
+type Product = {
+      id: 2,
+      title: 'Mens Casual Premium Slim Fit T-Shirts ',
+      price: 22.3,
+      description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+      category: "men's clothing",
+      image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+      rating: [Object]
+}
+type Props = {
+  products: Product[]
+}
+export default function Home({products}: Props) {
   return (
+  /* Esto se ejecuta del lado del (cliente) navegador y es para mostrar en la pantalla los datos obtenidos de la API*/
     <>
       <Head>
         <title>eCommerce Project</title>
@@ -11,8 +25,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <Heading>Hello World</Heading>
+        <ol>
+        {products.map(product => {
+          return <li key={product.id}>{product.title}</li>
+        })}
+        </ol>
       </main>
     </>
   )
+}
+
+/* Esto se ejecuta del lado del servidor y es para obtener los datos de la API*/
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const products = await fetch('https://fakestoreapi.com/products')
+  .then((res) => res.json())
+
+  return {
+    props: {
+      products
+    }
+  }
 }
